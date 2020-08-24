0788bac200cc82a195f5c80aaaba8e7d8619f26b
Oğuz Ergül <erguloguz@hotmail.com>
2020-08-24 18:18:31 +0300

    view hazırlandı

---
+++ src/views/Users/SetUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <SetUserForm />
+  </div>
+</template>
+
+<script>
+import SetUserForm from "@/components/forms/SetUserForm";
+export default {
+  name:"SetUser",
+  components: {SetUserForm},
+}
+</script>
+
+<style scoped>
+
+</style>
